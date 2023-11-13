92906b7b8eac4281c19cfd83a1e810892f02b4c5
juanjosegr <juanjosegr9@gmail.com>
2023-11-13 17:25:27 +0100

    transforma a sha256

--- fuerzaBruta/Program.cs
+++ fuerzaBruta/Program.cs
@@ -1,29 +1,66 @@
 ﻿using System;
 using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
 
 class Program
 {
     static void Main()
     {
-        //Ruta del archivo a leer.
-        string filePath = "ruta-del-archivo.txt";
+        string contrasenaBuscar = "ma930520";
 
-        //comprobar si existe.
+        BuscarContraseñaEnArchivo(contrasenaBuscar);
+
+        string contraseñaEncriptada = ObtenerHashSHA256(contrasenaBuscar);
+
+        Console.WriteLine($"Contraseña encriptada (SHA-256): {contraseñaEncriptada}");
+    }
+
+    static void BuscarContraseñaEnArchivo(string contrasenaBuscar)
+    {
+        string filePath = "C:\\Users\\juanj\\RiderProjects\\fuerzaBruta\\2151220-passwords.txt";
+
         if (File.Exists(filePath))
         {
-            //Utilizar el bloque ussing para garantizar el StreamReader se cierre
             using (StreamReader sr = new StreamReader(filePath))
             {
                 string line;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    Console.WriteLine(line);
+                    //Console.WriteLine(line);
+                    if (line == contrasenaBuscar)
+                    {
+                        Console.WriteLine($"Contraseña encontrada:\nLa contraseña es {line}");
+                        break;
+                    }
                 }
             }
         }
         else
         {
             Console.WriteLine("El archivo no existe.");
+        }
+    }
+
+    static string ObtenerHashSHA256(string contrasena)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            //Convertir entrada a byte
+            byte[] bytes = Encoding.UTF8.GetBytes(contrasena);
+
+            //Calcular el hash
+            byte[] hashBytes = sha256.ComputeHash(bytes);
+
+            //Convierte el hash en una cadena hexadecimal.
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < hashBytes[i]; i++)
+            {
+                builder.Append(hashBytes[i].ToString("x2"));
+            }
+
+            return builder.ToString();
         }
     }
 }
