c36d77728eb3c20b3b12749ec5a0865ce392c9e7
juanjosegr <juanjosegr9@gmail.com>
2023-11-13 16:33:12 +0100

    hola mundo

---
+++ fuerzaBruta/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        //Ruta del archivo a leer.
+        string filePath = "ruta-del-archivo.txt";
+
+        //comprobar si existe.
+        if (File.Exists(filePath))
+        {
+            //Utilizar el bloque ussing para garantizar el StreamReader se cierre
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("El archivo no existe.");
+        }
+    }
+}
