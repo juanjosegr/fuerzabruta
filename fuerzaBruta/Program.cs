d120fc2a3060ab5818a8548db2320156edb62538
juanjosegr <juanjosegr9@gmail.com>
2023-11-16 16:31:04 +0100

    finish

--- fuerzaBruta/Program.cs
+++ fuerzaBruta/Program.cs
@@ -4,23 +4,31 @@ using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 
-class Program
+internal class Program
 {
-    static void Main()
+    private static void Main()
     {
-        string contrasenaBuscar = "ma930520";
-
-        BuscarContraseñaEnArchivo(contrasenaBuscar);
-
-        string contraseñaEncriptada = ObtenerHashSHA256(contrasenaBuscar);
-
-        Console.WriteLine($"Contraseña encriptada (SHA-256): {contraseñaEncriptada}");
-    }
-
-    static void BuscarContraseñaEnArchivo(string contrasenaBuscar)
-    {
+        var contrasenaBuscar = "ma930520";
         string filePath = "C:\\Users\\juanj\\RiderProjects\\fuerzaBruta\\2151220-passwords.txt";
 
+        BuscarContraseñaEnArchivo(contrasenaBuscar, filePath);
+
+        var contraseñaEncriptada = EncriptarPaswHash256(contrasenaBuscar);
+        Console.WriteLine($"Contraseña encriptada (SHA-256): {contraseñaEncriptada}");
+
+        List<string> contraseñasEncriptadas = EncriptarDocumentoHash256(filePath);
+        if (contraseñasEncriptadas.Contains(contraseñaEncriptada))
+        {
+            Console.WriteLine("Contraseña encontrada.");
+        }
+        else
+        {
+            Console.WriteLine("No encontrada.");
+        }
+    }
+
+    private static void BuscarContraseñaEnArchivo(string contrasenaBuscar, string filePath)
+    {
         if (File.Exists(filePath))
         {
             using (StreamReader sr = new StreamReader(filePath))
@@ -43,7 +51,7 @@ class Program
         }
     }
 
-    static string ObtenerHashSHA256(string contrasena)
+    private static string EncriptarPaswHash256(string contrasena)
     {
         using (SHA256 sha256 = SHA256.Create())
         {
@@ -55,7 +63,8 @@ class Program
 
             //Convierte el hash en una cadena hexadecimal.
             StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < hashBytes[i]; i++)
+            
+            for (int i = 0; i < hashBytes.Length; i++)
             {
                 builder.Append(hashBytes[i].ToString("x2"));
             }
@@ -63,4 +72,36 @@ class Program
             return builder.ToString();
         }
     }
+
+    private static List<string> EncriptarDocumentoHash256(String filePath)
+    {
+        List<string> hashes = new List<string>();
+
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string? line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(line);
+
+                    byte[] hashBytes = sha256.ComputeHash(bytes);
+
+                    StringBuilder builder = new StringBuilder();
+                    for (int i = 0; i < hashBytes.Length; i++)
+                    {
+                        builder.Append(hashBytes[i].ToString("x2"));
+                    }
+
+                    hashes.Add(builder.ToString());
+                }
+            }
+        }
+
+        return hashes;
+    }
+    
+    
+    
 }
